964a9f9d7fc7e96d83f688b8c7a450f63f6f78dd
pravboi <pravachanpatra@gmail.com>
2024-06-17 20:12:54 -0500

    choero deployment configurations

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
+
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
@@ -10,3 +13,6 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,6 +62,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
 ]
+# AUTH_USER_MODEL = "api.User"   # defining the custom-user-model app.ModelName
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -100,8 +102,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        "USER":os.getenv("DB_USER"),
+        "PASSWORD":os.getenv("DB_PWD"),
+        "HOST":os.getenv("DB_HOST"),
+        "PORT":os.getenv("DB_PORT"),
     }
 }
 
